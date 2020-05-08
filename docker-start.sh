edd6a925002807dbc78832e5d4447a40d30f6b00
Slawek <slawek.ziel0401@gmail.com>
2020-05-08 11:47:17 +0200

    dodanie uruchamiania redisa do skryptu docer-start

--- docker-start.sh
+++ docker-start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 
 #
 # Skrypt do uruchomienia docker'a i kontenerów z bazami danych
@@ -7,3 +7,5 @@
 systemctl start docker.service
 ./src/database/start.sh main
 ./src/database/start.sh user
+
+docker run -p 6379:6379 -d redis
